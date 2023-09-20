dee00f177431364e19ef4ee5cc5dae19f3f6ccea
Markus Heinemann <markus.heinemann@trustify.ch>
2023-09-20 15:33:23 +0200

    update meta description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,15 @@
-import Head from "next/head";
+import { Metadata } from "next";
 import { Hero } from "../components/Hero";
+
+export const metadata: Metadata = {
+  title: "Markus Heinemann - DevOps & Software Architecture Specialist",
+  description:
+    "Personal website of Markus Heinemann, a dedicated software developer with expertise in infrastructure automation, DevOps, and software architecture. Explore projects, insights, and get in touch.",
+};
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <meta
-          name="title"
-          content="Markus Heinemann - DevOps & Software Architecture Specialist"
-        />
-        <meta
-          name="description"
-          content="Personal website of Markus Heinemann, a dedicated software developer with expertise in infrastructure automation, DevOps, and software architecture. Explore projects, insights, and get in touch."
-        />
-        <meta
-          name="keywords"
-          content="Markus Heinemann, software developer, infrastructure automation, DevOps, software architecture, personal website, tech blog, sport, dresden"
-        />
-        <meta name="author" content="Markus Heinemann" />
-      </Head>
       <Hero />
     </>
   );
